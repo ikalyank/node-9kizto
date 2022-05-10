64fe5c156e6647236090cb7feed3ad6ed20c1b86
ikalyank <kalyan29122000@gmail.com>
2022-05-10 23:05:52 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# node-9kizto
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/node-9kizto)
---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'giphy-api-testing-app';
+  constructor() { }
+  ngOnInit():void {
+}
+}
